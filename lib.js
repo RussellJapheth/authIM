3185e14599d10ff5bca6a8746161021074ebba47
Russell Japheth <japheth.russell.i@gmail.com>
2022-10-25 05:22:11 +0100

    feat: setup library entry point

---
+++ lib.js
@@ -0,0 +1,13 @@
+const Users = require("./src/Users");
+const Roles = require("./src/Roles");
+const Permissions = require("./src/Permissions");
+const Groups = require("./src/Groups");
+const AuthIM = require("./src/AuthIM");
+
+module.exports = {
+    AuthIM,
+    Users,
+    Roles,
+    Permissions,
+    Groups,
+};
